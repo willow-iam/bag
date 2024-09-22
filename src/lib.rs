d45c0ddcaa00b1e6f44ac8e7961a0cd9e9d117f4
= <=>
2024-09-22 16:10:50 -0400

    Rethought ArrayBag data structure * Creating a macro for impling FromIter for ArrayBag<T,R> was annoying because it required using impl<T,R> rather than just impl<t>, so instead I'm just impling Bag for Vec. * rng is now a parameter to pop, rather than part of the data structure.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-use rand::{Rng,SeedableRng};
+use rand::Rng;
 
 pub trait Bag<T> {
     fn push(&mut self,item:T); //Add an item to the bag.
-    fn pop(&mut self)->Option<T>; //Remove a random(determined by RNG) item from the bag
+    fn pop(&mut self, rng:impl Rng)->Option<T>; //Remove a random(determined by RNG) item from the bag
     fn new_empty()->Self; //New empty bag
     fn multipush(&mut self, iter:impl IntoIterator<Item=T>){
         for item in iter{self.push(item);}
@@ -10,19 +10,19 @@ pub trait Bag<T> {
 }
 
 
-/*Perhaps todo
-#[macro_export]
-macro_rules! bag_f_iter {
-    ($new_bag:ty) => {
-        impl<T> FromIterator<T> for new_bag<T>{
-            let nb = Self::new_empty();
-            nb.multipush(iter);
-            return nb;
+/* #[macro_export]
+macro_rules! impl_fromiter {
+    ($t:ty, $new_bag:ty) => {
+        impl<$t>  FromIterator<$t> for $new_bag{
+            fn from_iter(iter:Iterator<$t>){
+                let nb = Self::new_empty();
+                nb.multipush(iter);
+                return nb;
+            }
         }
     };
-}
-*/
-
+} */
+/*
 pub struct ArrayBag<T,R:SeedableRng>{
     items : Vec<T>,
     rng : R,
@@ -43,16 +43,23 @@ impl<T,R:SeedableRng+Rng> Bag<T> for ArrayBag<T,R>{
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use rand::rngs::StdRng;
-
-    use super::*;
-
-    #[test]
-    fn empty_pop(){
-        let mut new_empty_bag:ArrayBag<u8,StdRng>=ArrayBag::new_empty();
-        assert_eq!(new_empty_bag.pop(), None);
+impl <T>FromIterator<T>for ArrayBag<T,R> where ArrayBag<T,R> : Bag<T>{
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let mut nb = Self::new_empty();
+        nb.multipush(iter);
+        return nb;
     }
-    
 }
+*/
+impl<T> Bag<T> for Vec<T>{
+    fn push(&mut self,item:T) {self.push(item)}
+
+    fn pop(&mut self, mut rng:impl Rng)->Option<T> {
+        if self.is_empty() { None }
+        else { Some(self.swap_remove(rng.gen_range(0..self.len()))) }
+    }
+
+    fn new_empty()->Self {vec![]}
+}
+
+
